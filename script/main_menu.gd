01b7e2afc3b49cf92222cf049f5b4cdc660b136b
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-05-14 18:29:30 +0800

    pause button

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scene/game.tscn")
+	get_tree().change_scene_to_file("res://scene/main_scene.tscn")
 
 func _on_setting_pressed():
 	print("Settings not implemented yet")
