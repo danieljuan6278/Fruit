1e6fdef06a1cd7c2c56671a39c444c57e5ee939c
iHoshiii <aldwinloreto690@gmail.com>
2026-05-15 11:43:42 +0800

    main menu sound looping

--- script/audio_manager.gd
+++ script/audio_manager.gd
@@ -6,7 +6,7 @@ var menu_snd = preload("res://assets/sounds/menu.wav")
 var main_play_snd = preload("res://assets/sounds/Kingdom at the Ready.wav")
 var buttons_snd = preload("res://assets/sounds/buttons.wav")
 var combo_snd = preload("res://assets/sounds/combo.wav")
-var game_over_snd = preload(	"res://assets/sounds/game_over.wav")
+var game_over_snd = preload("res://assets/sounds/game_over.wav")
 
 var bgm_player: AudioStreamPlayer
 var sfx_players: Array = []
@@ -18,14 +18,28 @@ func _ready():
 	add_child(bgm_player)
 	bgm_player.bus = "Master"
 	
+	# Connect finished signal for unlimited looping
+	bgm_player.finished.connect(_on_bgm_finished)
+	
 	for i in range(8):
 		var p = AudioStreamPlayer.new()
 		add_child(p)
 		sfx_players.append(p)
 
+func _on_bgm_finished():
+	# This ensures the current background music loops forever
+	bgm_player.play()
+
 func play_main_bgm():
 	if bgm_player.stream != main_play_snd:
 		bgm_player.stream = main_play_snd
+		bgm_player.play()
+	elif not bgm_player.playing:
+		bgm_player.play()
+
+func play_menu_music():
+	if bgm_player.stream != menu_snd:
+		bgm_player.stream = menu_snd
 		bgm_player.play()
 	elif not bgm_player.playing:
 		bgm_player.play()
@@ -40,31 +54,14 @@ func play_sfx(stream: AudioStream):
 		sfx_players[0].stream = stream
 		sfx_players[0].play()
 
-func play_success():
-	play_sfx(success_snd)
-
-func play_failed():
-	play_sfx(failed_snd)
-
-func play_buttons():
-	play_sfx(buttons_snd)
-
-func play_combo():
-	play_sfx(combo_snd)
-
-func play_game_over():
-	play_sfx(game_over_snd)
-
-func play_menu_music():
-	if bgm_player.stream != menu_snd:
-		bgm_player.stream = menu_snd
-		bgm_player.play()
-	elif not bgm_player.playing:
-		bgm_player.play()
+func play_success(): play_sfx(success_snd)
+func play_failed(): play_sfx(failed_snd)
+func play_buttons(): play_sfx(buttons_snd)
+func play_combo(): play_sfx(combo_snd)
+func play_game_over(): play_sfx(game_over_snd)
 
 func stop_music():
 	bgm_player.stop()
 
 func set_master_volume(volume: float):
-	"""Set master volume (0.0 to 1.0)"""
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(volume))
